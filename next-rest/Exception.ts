712f18bba93f44f9618dbf6f262ee9f1e6c8c6fd
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-03-31 21:36:56 +0200

    Updated the tsconfig.json to include the next-rest folder. This is so that the next-rest folder is compiled into the dist folder. This is so that the next-rest folder can be used as a module in other projects.

--- next-rest/Exception.ts
+++ next-rest/Exception.ts
@@ -4,7 +4,7 @@ export class Exception extends Error {
   public readonly request: FetchRequest;
   public readonly response: Response;
 
-  constructor (message: string, request: FetchRequest, response: Response) {
+  constructor(message: string, request: FetchRequest, response: Response) {
     super(message);
     this.request = request;
     this.response = response;
