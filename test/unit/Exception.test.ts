620cc716b119e337fc78e996e975427b285bff1c
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-01 12:42:45 +0200

    Looks like this setup will work.

--- test/unit/Exception.test.ts
+++ test/unit/Exception.test.ts
@@ -8,4 +8,11 @@ describe('Exception', () => {
     expect(exception.request).toBe(request);
     expect(exception.response).toBe(response);
   });
+
+  it('should assign the message passed to it', () => {
+    const request = new Request('http://example.com');
+    const response = new Response('Hello World');
+    const exception = new Exception('Hello World', request, response);
+    expect(exception.message).toBe('Hello World');
+  });
 });
