bbf89fae98c90e9e2853517b9f722fa258838288
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-08 18:12:30 +0200

    wip

--- test/component/Resource/Resource.test.ts
+++
@@ -1,8 +0,0 @@
-import TestDataObject from './TestResource';
-
-describe('DataObject', () => {
-  it('should be able to create a DataObject', () => {
-    const dataObject = new TestDataObject();
-    expect(dataObject).toBeTruthy();
-  });
-});
--- test/component/Resource/TestResource.ts
+++
@@ -1,2 +0,0 @@
-import Resource from '#Resource';
-import {Model} from './Model';
