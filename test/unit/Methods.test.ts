a8d76366c7d7fa652e6a5c5b6c1d50e99b035f36
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-15 01:21:23 +0200

    better method tests

--- test/unit/Methods.test.ts
+++ test/unit/Methods.test.ts
@@ -1,12 +1,26 @@
-import {isHttpMethod, httpMethods} from '#Methods';
+import {isHttpMethod} from '#Methods';
 
 describe('Methods.ts', () => {
   describe('.isHttpMethod()', () => {
-    test.each(httpMethods)('Candidate %s', candidate => {
-      expect(isHttpMethod(candidate)).toBe(true);
+    it('returns true for a supported HTTP method.', () => {
+      expect(isHttpMethod('GET')).toBe(true);
+      expect(isHttpMethod('POST')).toBe(true);
+      expect(isHttpMethod('PUT')).toBe(true);
+      expect(isHttpMethod('DELETE')).toBe(true);
     });
-    test.each(['asdf', 'qwer', 'yxcv'])('candidate: %s', candidate => {
-      expect(isHttpMethod(candidate)).toBe(false);
+    it('returns false for an unsupported HTTP method.', () => {
+      expect(isHttpMethod('PATCH')).toBe(false);
+      expect(isHttpMethod('HEAD')).toBe(false);
+      expect(isHttpMethod('OPTIONS')).toBe(false);
+      expect(isHttpMethod('')).toBe(false);
+      expect(isHttpMethod('FOO')).toBe(false);
+      expect(isHttpMethod('get')).toBe(false);
+      expect(isHttpMethod('post')).toBe(false);
+      expect(isHttpMethod('put')).toBe(false);
+      expect(isHttpMethod('patch')).toBe(false);
+      expect(isHttpMethod('delete')).toBe(false);
+      expect(isHttpMethod('head')).toBe(false);
+      expect(isHttpMethod('options')).toBe(false);
     });
   });
 });
